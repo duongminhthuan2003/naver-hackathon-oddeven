f58434186a34ff468d68ce075311d133e3720b1f
duongminhthuan2003 <thuan.duongminh2003@gmail.com>
2025-10-19 18:19:40 +0700

    update hover state for squares

--- oddeven-client/src/components/square.tsx
+++ oddeven-client/src/components/square.tsx
@@ -1,6 +1,6 @@
 function Square({value, handleClick, isWinning} : { value: number, handleClick: () => void, isWinning?: boolean }) {
     return (
-        <button className={`flex h-16 w-16 justify-center items-center rounded-lg ${isWinning ? "bg-yellow-200" : ((value > 0 ? (value % 2 === 0 ? "bg-green-200" : "bg-blue-200") : "bg-gray-200"))} transition-all`} onClick={() => {handleClick()}}>
+        <button className={`flex h-16 w-16 justify-center items-center rounded-lg ${isWinning ? "bg-yellow-200" : ((value > 0 ? (value % 2 === 0 ? "bg-green-200" : "bg-blue-200") : "bg-gray-200 hover:bg-gray-300"))} transition-all`} onClick={() => {handleClick()}}>
             {value}
         </button>
     )
